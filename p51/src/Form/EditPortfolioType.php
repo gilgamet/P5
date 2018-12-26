81f61f078563767917cb275c6bc8858266b8abc7
gilgamet <gilgamet@free.fr>
2018-12-26 17:54:15 +0100

    ajout de mesage de confirmation form

---
+++ p51/src/Form/EditPortfolioType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Portfolio;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EditPortfolioType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', null, [
+                'label' => 'Titre'
+            ])
+            ->add('language', null, [
+                'label' => 'Langage utilisé'
+            ])
+            ->add('description', null, [
+                'label' => 'Description du projet'
+            ])
+            ->add('cahier_des_charges', null, [
+                'label' => 'Cahier des charges'
+            ])
+            ->add('url', null, [
+                'label' => 'lien du site'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Portfolio::class,
+        ]);
+    }
+}
